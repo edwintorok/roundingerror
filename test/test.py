45e67cf95daecadca8c686cb97b51ac38a5d0c8a
Edwin Török <edwin@etorok.net>
2024-09-11 23:58:56 +0100

    docu

--- test/test.py
+++ test/test.py
@@ -3,28 +3,34 @@
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles,FallingEdge,RisingEdge,First,Edge
+from cocotb.triggers import ClockCycles, Edge, FallingEdge, First, RisingEdge
+
 
 def time(unit) -> int:
     return cocotb.utils.get_sim_time(unit)
 
+
 def time_us():
-    return time('us')
+    return time("us")
+
 
 def time_ms():
-    return time('ms')
+    return time("ms")
+
 
 def time_clocks(clock):
-    return time('step') / clock.period
+    return time("step") / clock.period
+
 
 async def wait_cycles(dut, clock, n, trigger):
     t0 = time_clocks(clock)
-    #await First(ClockCycles(clock.signal, n), trigger)
+    # await First(ClockCycles(clock.signal, n), trigger)
     await trigger
     t1 = time_clocks(clock)
     delta = t1 - t0
     dut._log.info("trigger signal: %d, after %f clocks", trigger.signal.value, delta)
     assert delta == n
+
 
 @cocotb.test()
 async def test_project(dut):
@@ -46,16 +52,16 @@ async def test_project(dut):
     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
-    #dut.ui_in.value = 20
-    #dut.uio_in.value = 30
+    # dut.ui_in.value = 20
+    # dut.uio_in.value = 30
 
     # Wait for one clock cycle to see the output values
-    #await ClockCycles(dut.clk, 1)
+    # await ClockCycles(dut.clk, 1)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
     # TODO
-    #assert dut.uo_out.value == 50
+    # assert dut.uo_out.value == 50
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
@@ -65,7 +71,7 @@ async def test_project(dut):
     hsync_end = RisingEdge(proj.hsync)
     vsync_start = FallingEdge(proj.vsync)
     vsync_end = RisingEdge(proj.vsync)
-    
+
     if proj.vsync.value != 1:
         await vsync_end
     if proj.hsync.value != 1:
@@ -86,21 +92,21 @@ async def test_project(dut):
     dut._log.info("hsync duration: %fus", h2 - h1)
 
     await ClockCycles(dut.clk, 48)
-    dut._log.info("line: %fus", time_us()-h0)
+    dut._log.info("line: %fus", time_us() - h0)
 
     await wait_cycles(dut, clock, (490 - 1) * 800 - 96 - 48, vsync_start)
     v1 = time_ms()
-    dut._log.info("vsync start: %fms", v1 - v0)   
+    dut._log.info("vsync start: %fms", v1 - v0)
     await wait_cycles(dut, clock, 2 * 800, vsync_end)
 
     v2 = time_ms()
-    dut._log.info("vsync duration: %fms", v2 - v1)   
+    dut._log.info("vsync duration: %fms", v2 - v1)
     await ClockCycles(dut.clk, 33 * 800 + 96 + 48)
     dut._log.info("frame: %fms", time_ms() - v0)
-    
+
     await hsync_start
     await vsync_start
-#    await vsync_end
-    await wait_cycles(dut, clock, 525*800, vsync_start)
+    #    await vsync_end
+    await wait_cycles(dut, clock, 525 * 800, vsync_start)
     await vsync_end
     await ClockCycles(dut.clk, 33 * 800)
