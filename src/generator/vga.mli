e6cfde90b3f1c09510d894939d90ee21370a6e1b
Edwin Török <edwin@etorok.net>
2024-10-26 16:34:22 +0100

    drop audio
    wasn't producing a useful signal

--- src/generator/vga.mli
+++ src/generator/vga.mli
@@ -13,7 +13,7 @@ open Config
 
 (** VGA controller output *)
 module O : sig
-  type 'a t = {srgb: 'a SRGB.t; hsync: 'a; vsync: 'a; audio: 'a}
+  type 'a t = {srgb: 'a SRGB.t; hsync: 'a; vsync: 'a}
   [@@deriving sexp_of, hardcaml]
 end
 
